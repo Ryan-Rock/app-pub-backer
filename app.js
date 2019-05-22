7ccc635efdff2935093bb9234cce22c37bfd85b3
xuezhr <17611251102@163.com>
2019-05-22 16:48:13 +0800

    接口管理工具的添加

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 var db=require('./routers/utils/db');
 var usersql=require('./routers/api/userinfo/login')
 const axios = require('axios') //引入axios
@@ -10,8 +11,13 @@ var allowCrossDomain = function (req, res, next) {
  res.header('Last-Modified', (new Date()).toUTCString());//自定义中间件，设置跨域需要的响应头。
  next();
 };
+app.use('/static', express.static('public'));
 app.use(allowCrossDomain);//运用跨域的中间件
 app.use(bodyParser.json());//运用中间件，对请求体的文本进行解析
+app.get('/', function (req, res) {
+      res.send('hello,菜鸟,路由输入(/static/index.html)');
+    });
+// app.use('/static', express.static('public'));
 // 登录注册
 // function abc(){
 
@@ -22,4 +28,5 @@ app.use(bodyParser.json());//运用中间件，对请求体的文本进行解析
 // }
 // abc()
 app.use('/userinfo',require('./routers/userinfo/login'));
+// app.use(express.static("public")).listen(3000, () => console.log('success'));
 app.listen(3000, () => console.log('success'))
